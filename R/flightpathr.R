fe14d30d757816c94dd0dea86fc63cf8cc1cda95
eamoncaddigan <eamon.caddigan@gmail.com>
2016-04-14 10:23:38 -0400

    Initial commit of the mostly-empty package.

---
+++ R/flightpathr.R
@@ -0,0 +1,8 @@
+#' flightpathr: Tools to analyze aircraft and flight path data.
+#'
+#' This package provides functions for analyzing flight track and flight path
+#' data. Useful for studies of pilot and controller performance.
+#'
+#' @docType package
+#' @name flightpathr
+NULL
