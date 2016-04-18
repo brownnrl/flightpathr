6dca78ae4d781a8a05ebe1fa486bb5870695461d
eamoncaddigan <eamon.caddigan@gmail.com>
2016-04-18 14:21:15 -0400

    More tests.

--- tests/testthat/test_distanceFromPath.R
+++ tests/testthat/test_distanceFromPath.R
@@ -9,9 +9,21 @@ path <- matrix(c(-75.0268, 39.7065,
                nrow = 3, byrow = TRUE,
                dimnames = list(c("17N", "N81", "KACY"),
                                c("lon", "lat")))
-trajectory <- rbind(geosphere::gcIntermediate(path[1, ], path[2, ], 5),
-                    geosphere::gcIntermediate(path[2, ], path[3, ], 6))
+
+fakeTrajectory <- function(waypoints) {
+  trajectoryList <- vector("list", 2*nrow(waypoints)-1)
+  trajectoryList[[1]] <- waypoints[1, ]
+  for (i in seq(2, nrow(waypoints))) {
+    trajectoryList[[(i-1)*2]] <- geosphere::gcIntermediate(path[i-1, ], path[i, ], 5)
+    trajectoryList[[(i-1)*2+1]] <- waypoints[i, ]
+  }
+  return(do.call(rbind, trajectoryList))
+}
+
+trajectory <- fakeTrajectory(path)
 
 test_that("non-deviating paths have small distances", {
   expect_true(all(distanceFromPath(trajectory, path) < 1))
+  expect_true(all(distanceFromPath(sp::SpatialPoints(trajectory), sp::SpatialPoints(path)) < 1))
+  expect_true(all(distanceFromPath(as.data.frame(trajectory), as.data.frame(path)) < 1))
 })
