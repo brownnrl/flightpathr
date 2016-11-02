3d7980afa36fde1ab212ad80d52c4942e002cc2d
eamoncaddigan <eamon.caddigan@gmail.com>
2016-11-02 12:35:01 -0400

    Changed thresholding algorithm.

--- R/identifyManeuvers.R
+++ R/identifyManeuvers.R
@@ -3,28 +3,27 @@
 #'
 #' @param trajectory A \code{flighttrajectory} object (or input coercable to
 #'   one) indicating the trajectory of an aircraft.
-#' @param loThresh A bearing change (in degrees per second)
-#' @param hiThresh A bearing change (in degrees per second); any time point
-#'   associated with a change in bearing greater than this value will definitely
-#'   be labeled a turn.
+#' @param bearingThresh A bearing change threshold (in degrees).
+#' @param turnThresh A turn rate threshold (in degrees per second).
 #'
 #' @return A logical vector indicating whether each timepoint can be considered
 #'   a turn.
 #'
+#' @details A period is marked as a turn if the turn rate is greater than
+#'   \code{turnThresh} and the total bearing change is greater than
+#'   \code{bearingThresh}.
+#'
 #' @export
-identifyBearingChanges <- function(trajectory, loThresh, hiThresh = NA) {
-  t <- as.flighttrajectory(trajectory)
+identifyBearingChanges <- function(trajectory, bearingThresh, turnThresh) {
+  traj <- as.flighttrajectory(trajectory)
 
   bearingChanges <- c(NA,
-                      angleDiff(t$bearing[1:(length(t$bearing)-1)],
-                                t$bearing[2:length(t$bearing)])) /
-    c(NA, diff(t$time))
+                      angleDiff(traj$bearing[1:(length(traj$bearing)-1)],
+                                traj$bearing[2:length(traj$bearing)])) /
+    c(NA, diff(traj$time))
 
-  isBearingChange <- hysteresisThresh(bearingChanges, loThresh, hiThresh)
-  # if (length(loThresh) > 1) {
-  #   isBearingChange <- isBearingChange |
-  #     hysteresisThresh(-bearingChanges, -min(loThresh), -min(hiThresh))
-  # }
+  isBearingChange <- identifyChanges(traj$bearing, bearingChanges,
+                                     bearingThresh, turnThresh)
 
   return(isBearingChange)
 }
@@ -34,49 +33,41 @@ identifyBearingChanges <- function(trajectory, loThresh, hiThresh = NA) {
 #'
 #' @param trajectory A \code{flighttrajectory} object (or input coercable to
 #'   one) indicating the trajectory of an aircraft.
-#' @param hiThresh An altitude change (in feet per second); any time point
-#'   associated with a change in altitude greater than this value will
-#'   definitely be labeled a climb or descent.
+#' @param altitudeThresh An altitude threshold (in feet).
+#' @param verticalSpeedThresh A vertical speed threshold (in feet per second).
 #'
 #' @return A logical vector indicating whether each timepoint can be considered
 #'   a climb or descent.
 #'
 #' @export
-identifyAltitudeChanges <- function(trajectory, loThresh, hiThresh = NA) {
-  t <- as.flighttrajectory(trajectory)
-  altitudeChanges <- c(NA, diff(t$altitude)) / c(NA, diff(t$time))
+identifyAltitudeChanges <- function(trajectory, altitudeThresh, verticalSpeedThresh) {
+  traj <- as.flighttrajectory(trajectory)
+  altitudeChanges <- c(NA, diff(traj$altitude)) / c(NA, diff(traj$time))
 
-  isAltitudeChange <- hysteresisThresh(altitudeChanges, loThresh, hiThresh)
-  # if (length(loThresh) > 1) {
-  #   isAltitudeChange <- isAltitudeChange |
-  #     hysteresisThresh(-altitudeChanges, -min(loThresh), -min(hiThresh))
-  # }
+  isAltitudeChange <- identifyChanges(traj$altitude, altitudeChanges,
+                                      altitudeThresh, verticalSpeedThresh)
 
   return(isAltitudeChange)
 }
 
 #' Identify the timepoints in a trajectory that correspond to a change in
-#' altitude.
+#' speed.
 #'
 #' @param trajectory A \code{flighttrajectory} object (or input coercable to
 #'   one) indicating the trajectory of an aircraft.
-#' @param hiThresh An altitude change (in feet per second); any time point
-#'   associated with a change in altitude greater than this value will
-#'   definitely be labeled a climb or descent.
+#' @param speedThresh A speed threshold (in knots).
+#' @param accelerationThresh An accelaration threshold (in knots per second).
 #'
 #' @return A logical vector indicating whether each timepoint can be considered
-#'   a climb or descent.
+#'   a change in speed
 #'
 #' @export
-identifySpeedChanges <- function(trajectory, loThresh, hiThresh = NA) {
-  t <- as.flighttrajectory(trajectory)
-  speedChanges <- c(NA, diff(t$groundspeed)) / c(NA, diff(t$time))
+identifySpeedChanges <- function(trajectory, speedThresh, accelerationThresh) {
+  traj <- as.flighttrajectory(trajectory)
+  speedChanges <- c(NA, diff(traj$groundspeed)) / c(NA, diff(traj$time))
 
-  isSpeedChange <- hysteresisThresh(speedChanges, loThresh, hiThresh)
-  # if (length(loThresh) > 1) {
-  #   isSpeedChange <- isSpeedChange |
-  #     hysteresisThresh(-speedChanges, -min(loThresh), -min(hiThresh))
-  # }
+  isSpeedChange <- identifyChanges(traj$groundspeed, speedChanges,
+                                   speedThresh, accelerationThresh)
 
   return(isSpeedChange)
 }
